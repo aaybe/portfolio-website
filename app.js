fa27353de695cbf4aaae4719eb12f9c09e416d7a
Ayrton Bello <59799942+aaybe@users.noreply.github.com>
2021-09-27 22:49:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
